818115500eabf34e900cdc335799c06e21d91983
jerome310 <jeromeyoung333@gmail.com>
2023-01-02 10:42:58 -0500

    updated tailwind setup locally

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,7 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-<<<<<<< HEAD
-      screens: {
-        'mobile': '500px',
-         
-  
-        'tablet': '900px',
-        
-  
-        'desktop': '1200px',
-        
-      },
-=======
     extend: {},
->>>>>>> b448ef5f822d41f269a178c150a6f601430b58a1
   },
   plugins: [],
 }
